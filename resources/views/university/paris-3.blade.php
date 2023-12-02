1353d1d59a02510d2134e2a854b7d25fb2e14c9c
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-02 14:37:02 +0100

    paris 3

--- resources/views/university/paris-3.blade.php
+++ resources/views/university/paris-3.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
-@section('title',"معرفی دانشگاه پاریس ۳")
+@section('title',"معرفی برترین دانشگاه های فرانسه | دانشگاه پاریس ۳")
+@section("keywords","دانشگاه پاریس ۳,دانشگاه سوربن نوول,دانشگاه های برتر فرانسه,دانشگاه های فرانسه,دانشگاه های پاریس,تحصیل در فرانسه,تحصیل در دانشگاه پاریس ۳,بهترین دانشگاه های فرانسه,رتبه دانشگاه پاریس ۳,رشته های دانشگاه پاریس ۳,امکانات دانشگاه پاریس ۳")
+@section("description","در این مقاله به معرفی دانشگاه پاریس ۳، یکی از بهترین دانشگاه های فرانسه می پردازیم. دانشگاه پاریس ۳ در سال ۱۹۷۰ تاسیس شد و در حال حاضر بیش از ۱۰۰۰۰ دانشجو در آن تحصیل می کنند. این دانشگاه در زمینه های علوم، مهندسی، پزشکی، هنر و علوم انسانی دارای رتبه های برتری در جهان است.")
 @section('content')
 
     <div class="page-title-area bg-paris-3">
@@ -14,7 +16,7 @@
                     </li>
                     <li>
                         <a href="/universities">
-                            دانشگاه ها</a></li>
+                            برترین دانشگاه ها فرانسه</a></li>
                     <li>دانشگاه پاریس ۳</li>
                 </ul>
             </div>
@@ -33,55 +35,18 @@
                             <ol id="board"></ol>
                         </div>
                         <div class="service-list service-card">
-                            <h4 class="service-details-title">اطلاعات مشتری</h4>
+                            <h4 class="service-details-title">ارتباط با ما</h4>
                             <ul>
                                 <li>
-                                    <a href="tel:021-12345678">
-                                        021-12345678
-                                        <i class='bx bx-phone-call bx-rotate-270'></i>
+                                    <a href="/consult">
+                                        درخواست مشاوره مهاجرت تحصیلی به فرانسه
+                                        <i class='bx bx-time'></i>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="mailto:hello@ecorik.com">
-                                        hello@ecorik.com
+                                    <a href="mailto:info@applyvipconseil.com">
+                                        info@applyvipconseil.com
                                         <i class='bx bx-envelope'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    ایران ، استان تهران ، میدان آزادی
-                                    <i class='bx bx-location-plus'></i>
-                                </li>
-                                <li>
-                                    9:00 عصر – 8:00 صبح
-                                    <i class='bx bx-time'></i>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="service-list service-card">
-                            <h4 class="service-details-title">دانلود بروشورها</h4>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (1)
-                                        <i class='bx bxs-cloud-download'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (2)
-                                        <i class='bx bxs-cloud-download'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (3)
-                                        <i class='bx bxs-cloud-download'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (4)
-                                        <i class='bx bxs-cloud-download'></i>
                                     </a>
                                 </li>
                             </ul>
@@ -92,8 +57,8 @@
                     <div class="service-details-wrap">
                         <h2>دانشگاه پاریس ۳ سوربن جدید: مرکزی برای آموزش و تحصیل</h2>
                         <div class="single-services-imgs mb-30">
-                            <img src="{{asset("assets/img/paris_3_university.jpg")}}"
-                                 alt="تصویر دانشگاه پاریس ۳ سوربن جدید">
+                            <img src="{{asset("assets/img/universities/Paris3/paris_3_university.webp")}}"
+                                 alt="معرفی برترین دانشگاه های فرانسه | دانشگاه پاریس ۳ سوربن جدید">
                         </div>
                         <h3>تأسیس و تاریخچه</h3>
                         <p>
@@ -125,8 +90,8 @@
                             خدمت می‌کند.
                         </p>
                         <div class="rooms-details mb-30">
-                            <img src="{{asset("assets/img/paris_3_university_1.jpg")}}"
-                                 alt="تصویر دانشگاه پاریس ۴ - سوربن">
+                            <img src="{{asset("assets/img/universities/Paris3/paris_3_university_1.webp")}}"
+                                 alt="معرفی برترین دانشگاه های فرانسه | دانشگاه پاریس ۳ سوربن جدید">
                         </div>
                         <h3>تنوع فرهنگی و بین‌المللی</h3>
                         <p class="mb-30">
@@ -214,8 +179,8 @@
                             <div class="row align-items-center">
                                 <div class="col-lg-6 col-md-6">
                                     <div class="service-list-img">
-                                        <img src="{{asset("assets/img/paris_3.jpg")}}"
-                                             alt="تصویر دانشگاه پاریس ۳ سوربن جدید">
+                                        <img src="{{asset("assets/img/universities/Paris3/paris_3_logo.webp")}}"
+                                             alt="معرفی برترین دانشگاه های فرانسه | دانشگاه پاریس ۳ سوربن جدید">
                                     </div>
                                 </div>
                                 <div class="col-lg-6 col-md-6">
