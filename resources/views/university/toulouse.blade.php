f0516069a2c8b4b96a2112ca6830acaa80ecee91
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-04 01:05:55 +0100

    Toulouse

--- resources/views/university/toulouse.blade.php
+++ resources/views/university/toulouse.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
-@section('title',"
-معرفی دانشگاه تولوز
-")
+@section('title',"معرفی برترین دانشگاه های فرانسه | معرفی دانشگاه تولوز")
+@section("keywords","دانشگاه تولوز,دانشگاه Paul Sabatier,دانشگاه های برتر فرانسه,دانشگاه های فرانسه,دانشگاه های تولوز,تحصیل در فرانسه,تحصیل در دانشگاه تولوز,بهترین دانشگاه های فرانسه,رتبه دانشگاه تولوز,رشته های دانشگاه تولوز,امکانات دانشگاه تولوز")
+@section("description","در این مقاله به معرفی دانشگاه تولوز، یکی از بهترین دانشگاه های فرانسه می پردازیم. دانشگاه تولوز در سال ۱۲۲۹ تاسیس شد و در حال حاضر بیش از ۴۵۰۰۰ دانشجو در آن تحصیل می کنند. این دانشگاه در زمینه های علوم، مهندسی، پزشکی، هنر و علوم انسانی دارای رتبه های برتری در جهان است.")
 @section('content')
 
     <div class="page-title-area bg-toulouse">
@@ -18,7 +18,7 @@
                     </li>
                     <li>
                         <a href="/universities">
-                            دانشگاه ها</a></li>
+                            برترین دانشگاه ها فرانسه</a></li>
                     <li>
                         دانشگاه تولوز
                     </li>
@@ -39,55 +39,18 @@
                             <ol id="board"></ol>
                         </div>
                         <div class="service-list service-card">
-                            <h4 class="service-details-title">اطلاعات مشتری</h4>
+                            <h4 class="service-details-title">ارتباط با ما</h4>
                             <ul>
                                 <li>
-                                    <a href="tel:021-12345678">
-                                        021-12345678
-                                        <i class='bx bx-phone-call bx-rotate-270'></i>
+                                    <a href="/consult">
+                                        درخواست مشاوره مهاجرت تحصیلی به فرانسه
+                                        <i class='bx bx-time'></i>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="mailto:hello@ecorik.com">
-                                        hello@ecorik.com
+                                    <a href="mailto:info@applyvipconseil.com">
+                                        info@applyvipconseil.com
                                         <i class='bx bx-envelope'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    ایران ، استان تهران ، میدان آزادی
-                                    <i class='bx bx-location-plus'></i>
-                                </li>
-                                <li>
-                                    9:00 عصر – 8:00 صبح
-                                    <i class='bx bx-time'></i>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="service-list service-card">
-                            <h4 class="service-details-title">دانلود بروشورها</h4>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (1)
-                                        <i class='bx bxs-cloud-download'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (2)
-                                        <i class='bx bxs-cloud-download'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (3)
-                                        <i class='bx bxs-cloud-download'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        دانلود فایل (4)
-                                        <i class='bx bxs-cloud-download'></i>
                                     </a>
                                 </li>
                             </ul>
@@ -98,7 +61,7 @@
                     <div class="service-details-wrap">
                         <h2>دانشگاه تولوز</h2>
                         <div class="single-services-imgs mb-30">
-                            <img src="{{asset("assets/img/toulouse_university.jpg")}}" alt="تصویر دانشگاه تولوز">
+                            <img src="{{asset("assets/img/universities/Toulouse/toulouse_university.webp")}}" alt="معرفی برترین دانشگاه های فرانسه | دانشگاه تولوز">
                         </div>
                         <h3>تاریخچه تأسیس</h3>
                         <p>
@@ -127,7 +90,7 @@
                             دانشگاه تولوز یک دانشگاه دولتی از برترین دانشگاه‌های تولوز، فرانسه است.
                         </p>
                         <div class="rooms-details mb-30">
-                            <img src="{{asset("assets/img/toulouse_university_1.jpg")}}" alt="تصویر دانشگاه تولوز">
+                            <img src="{{asset("assets/img/universities/Toulouse/toulouse_university_1.webp")}}" alt="معرفی برترین دانشگاه های فرانسه | دانشگاه تولوز">
                         </div>
                         <h3>رتبه‌بندی جهانی</h3>
                         <p>
@@ -246,7 +209,7 @@
                             <div class="row align-items-center">
                                 <div class="col-lg-6 col-md-6">
                                     <div class="service-list-img">
-                                        <img src="{{asset("assets/img/toulouse.png")}}" alt="دانشگاه تولوز">
+                                        <img src="{{asset("assets/img/universities/Toulouse/toulouse_logo.webp")}}" alt="معرفی برترین دانشگاه های فرانسه | دانشگاه تولوز">
                                     </div>
                                 </div>
                                 <div class="col-lg-6 col-md-6">
